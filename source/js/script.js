3b8e32ef704cefb5c0a69565d84849ac4a0e26b6
Kim Pham <ms.kimpham@gmail.com>
2015-04-25 19:35:59 -0700

    Header, landing, and single guest

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,19 @@
-(function() {
-  $('#test-js').click(function(e) {
-    e.preventDefault();
-    alert('It works!');
+$(document).ready(function() {
+  var shown = false;
+  $(".guest-image").click(function() {
+    // refactor to target specific episode guest
+    var parentElem = $(this).parents("#guests-container");
+    if (shown === false) {
+      parentElem.css("top", 0);
+      $(this).css("background", "url(../img/guests/001-alex-on@2x.png) no-repeat center");
+      $(this).css("background-size", "contain");
+      shown = true;
+    } else {
+      $(this).css("background", "url(../img/guests/001-alex-off@2x.png) no-repeat center");
+      $(this).css("background-size", "contain");
+      parentElem.css("top", "");
+      shown = false;
+    }
   });
-})();
+
+});
