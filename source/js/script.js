829654b3c14bd65a5e45f360b3ad9805a3a86fb6
Kim Pham <ms.kimpham@gmail.com>
2015-05-03 16:21:59 -0700

    Refactor JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,68 +1,17 @@
 $(document).ready(function() {
-  var podcastInfo = {
-  koberger: {
-    episode: "004",
-    guest: "Gregory Koberger",
-    company: "ReadMe.io",
-    ipa: "Pliney the Elder",
-    duration: 42
-  },
-  amber: {
-    episode: "003",
-    guest: "Amber Feng",
-    company: "Stripe",
-    ipa: "Owlbert IPA",
-    duration: 35
-  },
-  alex: {
-    episode: "002",
-    guest: " Alex Maccaw",
-    company: "Stripe",
-    ipa: "Owlbert IPA",
-    duration: 30
-  },
-  kin: {
-    episode: "001",
-    guest: "Kin Lane",
-    company: "API Evangelist",
-    ipa: "Owlbert IPA",
-    duration: 40
-  }};
 
-  var soundcloud = false;
-  
-  $(".guest-image").click(function() {
-    // soundcloud player not showing => show it
-    var guest = $(this).parent(".guest").attr("id");
-
-    $("#soundcloud").find(".episode").html(podcastInfo[guest].episode + " ");
-    $("#soundcloud").find(".guest-name").html(podcastInfo[guest].guest);
-    $("#soundcloud").find(".company").html(podcastInfo[guest].company);
-    $("#soundcloud").find(".time").html(podcastInfo[guest].duration + " minutes");
-    $("#soundcloud").find(".ipa-name").html(podcastInfo[guest].ipa);
-
-    if ( soundcloud === false ) {
-      soundcloud = true;
-      $(this).addClass("active");
-      $(".guest-image").not(this).css("opacity", 0.3);
-      $(this).parents("#guests-container").css("top", 0);
-      $("#soundcloud").css("display", "block");
-    // soundcloud player is showing for selected guest => close it
-    } else if ( soundcloud === true && $(this).hasClass("active") ) {
-      soundcloud = false;
+  $(".guest-container").click(function() {
+    if ( $(this).hasClass("active") ) {
       $(this).removeClass("active");
-      $(".guest-image").css("opacity", 1.0);
-      $(this).parents("#guests-container").css("top", "");
-      $("#soundcloud").css("display", "none");
-    // soundcloud player showing, but different guest selected
-    } else if ( soundcloud === true && !$(this).hasClass("active") ) {
-      $(".guest-image").removeClass("active");
-      $(this).addClass("active");
-      $(this).css("opacity", 1.0);
-      $(".guest-image").not(this).css("opacity", 0.3);
-      $(".guest-image").not(this).css("opacity", 0.3);
-      $(this).parents("#guests-container").css("top", 0);
-      $("#soundcloud").css("display", "block");
+      $("#main-content").animate({bottom: -120}, "easeOutQuint");
+    } else {
+      var guest = $(this).data("guest");
+      $(".guest-container").removeClass("active");
+      $(".soundcloud").removeClass("active");
+      
+      $(this).removeClass("inactive").addClass("active");
+      $(".soundcloud[data-guest=" + guest + "]").addClass("active");
+      $("#main-content").animate({bottom: 0}, "easeOutQuint");
     }
   });
 
