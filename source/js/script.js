a32a7b2fe7dce5046dfad5c7c23f1c05644bc939
Kim Pham <ms.kimpham@gmail.com>
2015-05-03 20:02:35 -0700

    Add images for non-retina screens

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $(".guest-container").click(function() {
     if ( $(this).hasClass("active") ) {
       // $(this).removeClass("active");
-      $(".guest-container").removeClass("inactive");
+      $(".guest-container").removeClass("inactive").removeClass("active");
       $(".guest-container").removeClass("active");
       $("#main-content").animate({bottom: -120}, "easeOutQuint");
     } else {
